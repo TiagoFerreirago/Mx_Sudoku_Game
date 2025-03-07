95cb8a2e31c5e00d86bbba62bbf6f4b82e0d80a8
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-03-07 12:50:38 -0300

    adding finishbutton template

--- src/br/com/mx/sudoku/custom/ui/button/FinishGameButton.java
+++ src/br/com/mx/sudoku/custom/ui/button/FinishGameButton.java
@@ -3,11 +3,10 @@ package br.com.mx.sudoku.custom.ui.button;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
-public class CheckGameStatusButton extends JButton{
+public class FinishGameButton extends JButton {
 
-    public CheckGameStatusButton(ActionListener actionListener){
-
-        this.setText("Verificar jogo");
+    public FinishGameButton(ActionListener actionListener){
+        this.setText("Concluir");
         this.addActionListener(actionListener);
     }
 }
