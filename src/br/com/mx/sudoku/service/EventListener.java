5759a37f93772fc030b4c312a3839baf79da65c3
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-03-07 16:04:53 -0300

    adding interface eventlistener and adding notifierservice

---
+++ src/br/com/mx/sudoku/service/EventListener.java
@@ -0,0 +1,6 @@
+package br.com.mx.sudoku.service;
+
+public interface EventListener {
+
+    void update(final EventEnum eventType);
+}
--- src/br/com/mx/sudoku/service/NotifierService.java
+++ src/br/com/mx/sudoku/service/NotifierService.java
@@ -1,5 +1,27 @@
 package br.com.mx.sudoku.service;
 
-public class NotifierService {
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NotifierService implements EventListener{
+
+    private final Map<EventEnum, List<EventListener>> listeners = new HashMap<>(){{
+        put(EventEnum.CLEAR_SPACE, new ArrayList<>());
+    }};
+
+    public void subscribe(final EventEnum eventType, EventListener listener){
+        var selectedListeners = listeners.get(eventType);
+        selectedListeners.add(listener);
+    }
+
+    public void notify(final EventEnum eventType){
+        listeners.get(eventType).forEach(l -> {
+
+            l.u
+        }); 
+    }
 
 }
